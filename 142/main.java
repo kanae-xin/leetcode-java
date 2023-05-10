ae653e4686475e7e0100164b256eb58f32337c3f
simon <kanaexin@gmail.com>
2023-05-11 00:43:59 +1000

    linked list

---
+++ 142/main.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+            if(fast!=null){
+                fast = fast.next;
+            }
+            if(fast==slow &&fast!=null){
+                slow = head;
+                while(fast != slow){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
